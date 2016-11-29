e449853569e5f3996b8ee661e8a62972bd25d955
Troy Roethig <troethig@emarketinginc.com>
2016-11-29 16:44:47 -0700

    second commit

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyD_bAof3ciyX6d70PPAsLX-zaG7f79Ykz0",
+    authDomain: "catch-of-the-day-68f15.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-68f15.firebaseio.com",
+});
+
+export default base;
